105d9cfda48172fe87e00ad9c7a71f65d77722a1
dzface <dzface031@gmail.com>
2024-12-10 15:55:55 +0900

    연결 해제 버튼 이벤트 작성 중

--- app.js
+++ app.js
@@ -25,13 +25,72 @@ app.use((req, res)=>{
   res.status(404).send("Not found");
 });
 
-// websocket server
-const ws = require("ws");
-const wss = new ws.Server({server});
+// 채팅요청 및 매칭 관리 ---------------------------------------
+const waitingQueue =[]; // 소켓이 들어가는 리스트 소켓이 곧 유저ID
+const chatRooms = {}; // 모든채팅방에 대한 정보 객체 { roomId: [socket1, socket2] }
 
+function createRoomNumber(){
+  // 나중에 중복확인하는 코드 추가
+  // 4~5자리 숫자 랜덤 생성
+  const number = parseInt((Math.random())*100000);
+  return number;
+};
+function manageChatRoom(roomNumber) {
+  // 유저1 소켓, 유저2소켓을 방번호 객체에 저장
+  const [user1, user2] = chatRooms[roomNumber];
+  // 메시지 보내기 함수
+  const sendMessage = (sender, receiver, message) =>{
+    receiver.send(JSON.stringify({type:"MESSAGE", room:roomNumber, message}));
+  };
+  // 메세지 받기
+  user1.on("message", (data)=>{
+    console.log(JSON.parse(data));
+    sendMessage(user1, user2, data.message); 
+  });
+  user2.on("message", data=>{
+    console.log(JSON.parse(data));
+    sendMessage(user2, user1, data.message); 
+  });
+  // 채팅 종료 로직
+  const handleDisconnect = (leavingUser, StayingUser) =>{
+    // 서버 로그
+    console.log(`채팅방 ${roomNumber}가 종료되었습니다.`);
+    // 프론트측으로 통신종료 메세지 전달
+    StayingUser.send(JSON.stringify({type: "SYSTEM", roomNumber, message:"상대방이 채팅을 떠났습니다."}));
+    delete chatRooms[roomNumber]; // 채팅관리 객체 삭제
+  };
+  user1.on("close", ()=> handleDisconnect(user1, user2));
+  user2.on("close", ()=> handleDisconnect(user2, user1));
+};
+// 채팅요청 및 매칭 관리 ---------------------------------------
+
+
+// websocket server create
+const ws = require("ws");
+const moment = require("moment");
+const wss = new ws.Server({server});
+// websocket method
 wss.on("connection", (socket, request)=>{
   console.log("웹소켓 생성");
-  socket.send(JSON.stringify("Server : 웹소켓이 연결되었습니다.")); //나에게 보내기
+  waitingQueue.push(socket); // 대기열에 소켓요청자 추가
+  if(waitingQueue.length>=2) {
+    const [user1, user2]= waitingQueue.splice(0,2); // 대기열에서 맨앞 두명 추출
+    const roomNumber = createRoomNumber(); // 방 번호 생성;
+    chatRooms[roomNumber] = [user1, user2]; // 방번호에 소켓 배정;
+    // 유저에게 메세지 전달
+    const chatData = {
+      type : "SYSTEM", // SYSTEM, MESSAGE, 
+      room : roomNumber, // int
+      message : "상대방과 연결되었습니다!", // String
+      time : moment(new Date()).format("h:mm A") // string
+    };
+    // 연결 이벤트메세지 전송
+    user1.send(JSON.stringify(chatData));
+    user2.send(JSON.stringify(chatData));
+    // 채팅메세지 및 연결종료 메세지 전달 함수
+    manageChatRoom(roomNumber);
+  };
+
   socket.on("message", (data)=>{
     const recivedData = JSON.parse(data);
     console.log(recivedData);
@@ -43,6 +102,8 @@ wss.on("connection", (socket, request)=>{
     console.log(`소켓통신 종료 코드 : ${code}`);
   });
 });
+
+
 
 
 
--- src/js/chat.js
+++ src/js/chat.js
@@ -1,14 +1,34 @@
 const URL = "localhost:5000";
 const ws = new WebSocket(`ws:${URL}/chat`);
-
 ws.onopen = (e)=>{
   //console.log(e);
   console.log("웹소켓이 연결 되었습니다.");
+  sendButton.textContent = "연결끊기";
 };
-
 //  ws.send(JSON.stringify("내가 보이니?"));
 ws.onmessage = (event)=>{console.log(event)};
 ws.onerror = (err)=>{console.log(`에러 발생 : ${err}`)};
-ws.onclose = (e)=>{
-  console.log(`웹소켓 통신종료 ${e}`);
+
+
+
+
+
+
+
+const sendButton = document.querySelector(".connect-button");
+sendButton.addEventListener("click", onClickSendButton);
+
+connectStateLisener();
+function onClickSendButton() {
+  connectStateLisener();
+};
+function connectStateLisener(){
+  // 연결 상태 일때 작동: 연결 끊고 버튼 이름 재연결로 변경 
+  if(ws.readyState === WebSocket.OPEN){ // code 0
+    ws.close(); // 웹소켓 통신 종료
+    sendButton.textContent = "재연결";
+  } else if(ws.readyState === WebSocket.CLOSED){
+    ws.onopen = (e)=> {console.log("재연결")};
+    sendButton.textContent = "연결끊기";
+  };
 };
